ec7c4c1d57892c9bd9f50bdf980af8edaf34d5de
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2022-07-10 14:58:49 +0200

    Start of Part 1 Lesson 15

--- complete-javascript-course-master/01-Fundamentals-Part-1/middle/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/middle/script.js
@@ -25,7 +25,17 @@ let job1 = "programmer";
 let job2 = 'teacher';
 
 console.log(myFirstJob)
-*/
+
+
+// Values and Variables Challenge
+
+let country = 'The Netherlands';
+let continent = 'Europe';
+let population = 17.44;
+
+let locationString = `I live in ${country} which is located in ${continent} and a population of ${population} M People`;
+
+console.log(locationString);
 
 // Second Lecture Code
 let javascriptIsFun = true;
@@ -45,3 +55,86 @@ console.log(typeof year);
 
 year = 1991;
 console.log(typeof year);
+
+let isIsland = false;
+let language;
+
+console.log(locationString + '\nIs it an island? ' + isIsland);
+
+
+// Lesson 13. Let Const Var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; // cant do this!! its constant
+
+// Always start with a const unless a variable needs to change later on!
+
+var job = 'programmer';
+
+job = 'teacher';
+
+// var is old never use it!!!
+
+lastName = 'Schmedtmann'; // terrible idea as it defines on the global object not within scope
+console.log(lastName);
+
+
+// Assignments Lesson 13
+
+const languageNetherlands = 'Dutch';
+console.log(languageNetherlands);
+
+
+// Lesson 14 Basic Operators
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operatores
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--; // oposite of ++
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+let populationNow = 14990000;
+let halfPopulation = populationNow / 2;
+populationNow = ++populationNow;
+console.log(populationNow);
+let populationFinland = 6000000;
+console.log(populationFinland >= populationNow);
+let averagePopulation = 33000000;
+console.log(populationNow <= averagePopulation);
+const description = 'Portugal is in Europe, and its 11M people speak Portugese';
+*/
+
+// Lesson 15 Operator Precedence 
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(now - 1991 > now - 2018);
+
