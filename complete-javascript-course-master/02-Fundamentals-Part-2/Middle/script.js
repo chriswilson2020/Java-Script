06f899cf34afdeed7917f503a5d057eb146270b7
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2022-07-22 14:28:19 +0200

    Before Lesson 42 introduction to Objects

--- complete-javascript-course-master/02-Fundamentals-Part-2/Middle/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/Middle/script.js
@@ -208,5 +208,107 @@ const checkWinner = function (aveDolphins, aveKoalas) {
 
 checkWinner(dolphinsAverage, koalasAverage);
 checkWinner(dolphinsAverage2, koalasAverage2);
-*/
 
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// You can change an element in an array even though it is defined as a constant but you can't change the entire array like below
+// friends = ['Bob', 'Alice'];
+
+const firstName = 'Jonas'
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise 
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
+}
+
+
+
+// Coding Challenge JS Fundamentals Part 2 Coding Challenge #3
+
+const calcTip = function (bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.2;
+    }
+
+}
+
+// A simpler calc tip
+const calcTip2 = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+// An even shorter calc tip Arrow function
+const calcTip3 = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[bills.length - 1] + tips[tips.length - 1]];
+console.log(totals);
+
+*/
