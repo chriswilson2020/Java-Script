5a50bdc43d700fe6319a9727842fa3a31c826a7b
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2022-08-03 16:35:30 +0200

    Start of Section 11

--- complete-javascript-course-master/10-Functions/Middle/script.js
+++ complete-javascript-course-master/10-Functions/Middle/script.js
@@ -225,3 +225,179 @@ addVAT2(100);
 */
 
 // Coding Challenge #1
+/* Let's build a simple poll app!
+ A poll has a question, and array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' objext below.
+
+ Your tasks:
+
+ 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things. 
+   1.1 Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+     What is your favourite programming language?
+    0: JavaScript
+    1: Python
+    2: Rust
+    3: C++
+    (Write option number)
+
+  1.2 Based on the input number, update the 'answers' array property. For example if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button. 
+
+3. Create a method 'displayResults' which displays the poll results.  The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If the type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+5. BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array; and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword looklike in this situation? 
+
+*/
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n ${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults('array');
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'string') {
+      console.log(this.answers);
+    } else {
+      console.log(`Poll results are: ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+*/
+
+/*
+const runOnce = function () {
+  console.log('This may never run again!');
+};
+
+runOnce();
+
+(function () {
+  console.log('This will never run again!');
+  // const isPrivate = 23;
+})();
+
+(() => console.log('This will also never run again'))();
+
+{
+  // This is also private
+  const isPrivate = 23;
+
+  // This is not
+  var notPrivate = 46;
+}
+
+// console.log(Private);
+console.log(notPrivate);
+*/
+
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+*/
+
+/*
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are three groups, each with ${perGroup} passengers`);
+  }, 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// Only has priority when line 368 is commented out
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+/////////////////////////////////////////
+// Coding challenge #2
+
+/*
+This is more of a thinking challenge than a coding challenge
+
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that changes the color of the delected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again!
+
+2. And now explain to yourself (or someone around you) why this worked! Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example. 
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
